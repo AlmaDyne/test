af16de9548606d6be5ef677a89859db4c08e326d
AlmaDyne <123851079+AlmaDyne@users.noreply.github.com>
2023-11-20 21:02:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 'use strict';
 
-const canvas1 = document.getElementById('canvas1');
+const url = 'https://webhook.site/9c354858-d037-48e2-8e14-34f8ce6b59b3';
+const canvas = document.getElementById('canvas');
+let formData = new FormData();
 let submitTimer = null;
+let exit = false;
 
-canvas1.onpointerdown = function(e) {
-    clearTimeout(submitTimer);
+canvas.onpointerdown = function(e) {
+    if (submitTimer) clearTimeout(submitTimer);
+    submitTimer = null;
 
     let firstTouch = true;
     draw(e);
@@ -15,15 +19,20 @@ canvas1.onpointerdown = function(e) {
     this.onpointermove = draw;
 
     this.onpointerup = () => {
-        submitTimer = setTimeout(submit, 5e3);
+        createForm();
+
+        submitTimer = setTimeout(() => {
+            submitForm();
+            submitTimer = null;
+        }, 3e3);
 
         this.onpointermove = () => false;
         this.onpointerup = () => false;
     };
 
     function draw(e) {
-        let canvasRect = canvas1.getBoundingClientRect();
-        let ctx = canvas1.getContext('2d');
+        let canvasRect = canvas.getBoundingClientRect();
+        let ctx = canvas.getContext('2d');
         let x = e.clientX - canvasRect.left;
         let y = e.clientY - canvasRect.top;
 
@@ -34,41 +43,33 @@ canvas1.onpointerdown = function(e) {
     }
 };
 
-async function submit() {
-    // Картинка 1 - Рисунок
-    let image1Blob = await new Promise(resolve => canvas1.toBlob(resolve, 'image/png'));
+window.onunload = function() {
+    if (submitTimer) {
+        exit = true;
+        submitForm();
+    }
+};
 
-    // Картинка 2 - Файл png
-    /*let canvas2 = document.createElement('canvas');
-    canvas2.width = image.clientWidth;
-    canvas2.height = image.clientHeight;
+async function createForm() {
+    let imageBlob = await new Promise(resolve => canvas.toBlob(resolve, 'image/png'));
 
-    let context = canvas2.getContext('2d');
-    context.drawImage(image, 0, 0);
+    formData.set("firstName", "John");
+    formData.set("lastName", "Bauer");
+    formData.set("image1", imageBlob, "drawing.png");
+}
 
-    let image2Blob = await new Promise(resolve => canvas2.toBlob(resolve, 'image/png'));*/
-
-    // Создание данных формы
-    let formData = new FormData();
-    formData.append("firstName", "John");
-    formData.append("lastName", "Bauer");
-    formData.set("image1", image1Blob, "drawing.png");
-    //formData.set("image2", image2Blob, "wallpaper.png");
-
-    // Генерация URL на https://webhook.site
-    let url = 'https://webhook.site/9c354858-d037-48e2-8e14-34f8ce6b59b3';
-
-    // Отправление данные формы
-    let response = await fetch(url, {
+async function submitForm() {
+    let options = {
         method: 'POST',
         body: formData
-    });
+    };
+    if (exit) options.keepalive = true;
 
-    // Получение ответа от сервера
+    let response = await fetch(url, options);
+
     if (response.ok) {
-        //console.log('Отправка рисунка прошла успешно');
-        //alert('Отправка рисунка прошла успешно');
+        console.log('+');
     } else {
-        console.log('Ошибка HTTP заголовка:' + response.status);
+        console.error('Ошибка HTTP заголовка:' + response.status);
     }
 }
